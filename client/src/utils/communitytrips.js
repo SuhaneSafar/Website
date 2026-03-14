6a9ffceec7c7c6064bd93156c7fe4d3435412349
Shivangi Srivastava <shivangi_s@me.iitr.ac.in>
2026-03-14 22:51:09 +0530

    community trips price updated

--- client/src/utils/communitytrips.js
+++ client/src/utils/communitytrips.js
@@ -2,7 +2,7 @@ const communityTrips = [
   {
     title: "Manali",
     duration: "3N/4D",
-    price: 5699,
+    price: 5999,
     image: "/Images/manali-main.png",
     registrationLink: "Not aviable",
     tags: ["Best Seller"]
@@ -10,7 +10,7 @@ const communityTrips = [
   {
     title: "Mussoorie",
     duration: "2N/3D",
-    price: 3999,
+    price: 4999,
     image: "/Images/mussoorie-main.jpg",
     registrationLink: "Not aviable",
     tags: ["Best Seller"]
@@ -18,7 +18,7 @@ const communityTrips = [
   {
     title: "Dharamshala",
     duration: "3N/4D",
-    price: 5799,
+    price: 5999,
     image: "/Images/dharamsala-main.png",
     registrationLink: "Not aviable",
     tags: []
@@ -26,7 +26,7 @@ const communityTrips = [
   {
     title: "Spiti",
     duration: "7N/8D",
-    price: 14999,
+    price: 15999,
     image: "/Images/spiti-main.png",
     registrationLink: "https://docs.google.com/forms/d/e/1FAIpQLSeKDjow8nG-DD1PiQpipQDDH04z461HE_Z9AOE1s74gFQv1DQ/viewform?usp=header",
     tags: []
@@ -34,7 +34,7 @@ const communityTrips = [
   {
     title: "Shimla",
     duration: "3N/4D",
-    price: 5499,
+    price: 5999,
     image: "/Images/shimla-main.png",
     registrationLink: "Not aviable",
     tags: []
